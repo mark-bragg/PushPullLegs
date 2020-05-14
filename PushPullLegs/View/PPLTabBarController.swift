13899e442c367b576aca9cc5f97638fa974c3781
Mark <mark.bragg90@gmail.com>
2020-05-13 23:12:21 -0700

    refactor: rename PPLTabBarController

--- PushPullLegs/View/PPLTabBarController.swift
+++ PushPullLegs/View/PPLTabBarController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class PPLTabBarControllerViewController: UITabBarController {
+class PPLTabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
