77781e91c7e4da033612a1f940a702fe12b37b8e
Mark <mark.bragg90@gmail.com>
2020-05-16 08:41:52 -0700

    fix create exercise from add exercise on the fly vc (regression caused by latest refactoring)

---
+++ PushPullLegs/View/Storyboards/StoryboardIdentifiers.swift
@@ -0,0 +1,13 @@
+//
+//  StoryboardIdentifiers.swift
+//  PushPullLegs
+//
+//  Created by Mark Bragg on 5/16/20.
+//  Copyright © 2020 Mark Bragg. All rights reserved.
+//
+
+import Foundation
+
+enum StoryboardIdentifiers: String {
+    case createExerciseViewController = "CreateExerciseViewController"
+}
