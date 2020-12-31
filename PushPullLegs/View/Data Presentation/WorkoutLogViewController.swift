5a46af7817aef48d4fb49efd5cd4e5dba0c452bb
Mark Bragg <mark.bragg90@gmail.com>
2020-12-30 22:13:11 -0800

    workout log sorts by date now

--- PushPullLegs/View/Data Presentation/WorkoutLogViewController.swift
+++ PushPullLegs/View/Data Presentation/WorkoutLogViewController.swift
@@ -43,9 +43,50 @@ class WorkoutLogViewController: PPLTableViewController {
         guard let header = container.headerView else { return nil }
         let leftLabel = header.subviews.first(where: { $0.frame.origin.x == 0 })!
         let rightLabel = header.subviews.first(where: { $0.frame.origin.x != 0 })!
+        containerizeDateLabel(header, rightLabel as! UILabel)
         leftLabel.frame = CGRect(x: 20, y: leftLabel.frame.origin.y, width: leftLabel.frame.width - 20, height: leftLabel.frame.height)
         rightLabel.frame = CGRect(x: leftLabel.frame.width + 20, y: rightLabel.frame.origin.y, width: rightLabel.frame.width - 20, height: leftLabel.frame.height)
         return container
+    }
+    
+    func containerizeDateLabel(_ headerView: UIView, _ label: UILabel) {
+        let container = UIView(frame: label.frame)
+        headerView.addSubview(container)
+        label.removeFromSuperview()
+        container.addSubview(label)
+        label.sizeToFit()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+        label.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        addArrowImage(label)
+        if let grs = container.gestureRecognizers, grs.count == 0 { return }
+        container.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(dateHeaderTapped)))
+        container.isUserInteractionEnabled = true
+    }
+    
+    func addArrowImage(_ label: UIView) {
+        let down = UIImage(systemName: "arrowtriangle.down.fill")
+        let up = UIImage(systemName: "arrowtriangle.up.fill")
+        let img = UIImageView(image: down, highlightedImage: up)
+        img.sizeToFit()
+        img.tintColor = .lightGray
+        label.addSubview(img)
+        dateArrow = img
+        dateArrow.isHighlighted = WorkoutLogViewModel.ascending
+        positionDateArrow(label)
+    }
+    
+    func positionDateArrow(_ label: UIView) {
+        dateArrow.translatesAutoresizingMaskIntoConstraints = false
+        dateArrow.centerYAnchor.constraint(equalTo: label.centerYAnchor).isActive = true
+        dateArrow.leadingAnchor.constraint(equalTo: label.trailingAnchor, constant: 10).isActive = true
+    }
+    
+    weak var dateArrow: UIImageView!
+    @objc func dateHeaderTapped() {
+        WorkoutLogViewModel.ascending = !WorkoutLogViewModel.ascending
+        dateArrow.isHighlighted = WorkoutLogViewModel.ascending
+        reload()
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -83,11 +124,15 @@ class WorkoutLogViewController: PPLTableViewController {
 class WorkoutLogViewModel: NSObject, PPLTableViewModel {
     var workouts = [Workout]()
     let formatter = DateFormatter()
+    static var ascending = false
     
     init(withDataManager dataManager: WorkoutDataManager = WorkoutDataManager()) {
         super.init()
         formatter.dateFormat = "MM/dd/yy"
         workouts = WorkoutDataManager().workouts()
+        if WorkoutLogViewModel.ascending {
+            workouts.reverse()
+        }
     }
     
     func rowCount(section: Int) -> Int {
