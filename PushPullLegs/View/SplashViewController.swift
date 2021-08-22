ff545b14ea46843068aa7904b0ccd098a9f8b838
Mark Bragg <mark.bragg90@gmail.com>
2021-08-22 11:17:30 -0700

    add background view to splash vc

--- PushPullLegs/View/SplashViewController.swift
+++ PushPullLegs/View/SplashViewController.swift
@@ -14,14 +14,22 @@ import UIKit
 
 class SplashViewController: UIViewController {
     
+    weak var backgroundView: UIView!
     weak var imageView: UIImageView!
     weak var delegate: SplashViewControllerDelegate?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .black
+        addBackgroundView()
         addImageView()
         view.frame = view.superview?.bounds ?? .zero
+    }
+    
+    private func addBackgroundView() {
+        let bgv = UIView(frame: view.bounds)
+        view.addSubview(bgv)
+        backgroundView = bgv
+        backgroundView.backgroundColor = .black
     }
     
     private func addImageView() {
@@ -36,7 +44,7 @@ class SplashViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         UIView.animate(withDuration: TimeInterval(0.67), delay: TimeInterval(0.33)) {
-            self.view.backgroundColor = .clear
+            self.backgroundView.alpha = 0
             self.imageView.frame = CGRect(x: self.view.frame.width/2, y: self.view.frame.height/2, width: 1, height: 1)
         } completion: { (b) in
             self.delegate?.splashViewControllerDidDisappear(self)
