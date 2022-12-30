5d5875a35a987e0f6574c89cd26d166f16799db7
Mark Bragg <mark.bragg90@gmail.com>
2022-12-29 16:45:40 -0800

    clean up changes

--- PushPullLegs/View/Working Out/Exercise/Set Performance/QuantityCollectionViewController.swift
+++ PushPullLegs/View/Working Out/Exercise/Set Performance/QuantityCollectionViewController.swift
@@ -21,24 +21,31 @@ class QuantityCollectionViewController: UIViewController, UITextFieldDelegate, P
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.isUserInteractionEnabled = true
+        view.backgroundColor = PPLColor.primary
         addStackView()
+        constrainStackView()
+        styleStackView()
     }
     
     func addStackView() {
         let stack = UIStackView(arrangedSubviews: [spacer, getLabel(), getTextField(), getButton(), spacer])
-        stack.isUserInteractionEnabled = true
-        stack.alignment = UIStackView.Alignment.center
-        stack.axis = NSLayoutConstraint.Axis.vertical
-        stack.distribution = UIStackView.Distribution.equalCentering
-        stack.spacing = 14
         view.addSubview(stack)
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
-        stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        stack.heightAnchor.constraint(equalToConstant: height).isActive = true
         stackView = stack
+    }
+    
+    func constrainStackView() {
+        stackView?.translatesAutoresizingMaskIntoConstraints = false
+        stackView?.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView?.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView?.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        stackView?.heightAnchor.constraint(equalToConstant: height).isActive = true
+    }
+    
+    func styleStackView() {
+        stackView?.alignment = UIStackView.Alignment.center
+        stackView?.axis = NSLayoutConstraint.Axis.vertical
+        stackView?.distribution = UIStackView.Distribution.equalCentering
+        stackView?.spacing = 14
     }
     
     func getLabel() -> UILabel {
@@ -48,50 +55,59 @@ class QuantityCollectionViewController: UIViewController, UITextFieldDelegate, P
         label = lbl
         return lbl
     }
+    
     func getTextField() -> UIView {
         let txtContainer = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: height/3))
         let txtFld = UITextField()
-        txtFld.borderStyle = .roundedRect
-        txtFld.textAlignment = .center
-        txtFld.translatesAutoresizingMaskIntoConstraints = false
-        txtFld.widthAnchor.constraint(equalToConstant: 180).isActive = true
-        txtFld.font = UIFont.systemFont(ofSize: 40)
         txtContainer.addSubview(txtFld)
-        txtFld.centerXAnchor.constraint(equalTo: txtContainer.centerXAnchor).isActive = true
-        txtFld.centerYAnchor.constraint(equalTo: txtContainer.centerYAnchor).isActive = true
         textField = txtFld
+        prepareTextField()
         return txtContainer
     }
+    
+    func prepareTextField() {
+        constraintTextField()
+        styleTextField()
+        textField?.delegate = self
+        textField?.addTarget(self, action: #selector(editingChanged(_:)), for: .editingChanged)
+    }
+    
+    func constraintTextField() {
+        guard let container = textField?.superview else { return }
+        textField?.translatesAutoresizingMaskIntoConstraints = false
+        textField?.widthAnchor.constraint(equalToConstant: 180).isActive = true
+        textField?.centerXAnchor.constraint(equalTo: container.centerXAnchor).isActive = true
+        textField?.centerYAnchor.constraint(equalTo: container.centerYAnchor).isActive = true
+    }
+    
+    func styleTextField() {
+        textField?.borderStyle = .roundedRect
+        textField?.textAlignment = .center
+        textField?.font = UIFont.systemFont(ofSize: 32)
+        textField?.backgroundColor = PPLColor.quaternary
+        textField?.autocorrectionType = .no
+    }
+    
     func getButton() -> UIView {
-        var config = UIButton.Configuration.borderedProminent()
-        config.baseBackgroundColor = .black
-        config.buttonSize = UIButton.Configuration.Size.large
-        let btn = UIButton(configuration: config)
+        let btn = UIButton(configuration: buttonConfig())
         btn.backgroundColor = .primary
-        btn.isUserInteractionEnabled = true
         btn.addTarget(self, action: #selector(buttonReleased(_:)), for: .touchUpInside)
-        btn.titleLabel?.font = UIFont.systemFont(ofSize: 15)
-        btn.translatesAutoresizingMaskIntoConstraints = false
+        btn.titleLabel?.font = UIFont.systemFont(ofSize: 16)
         button = btn
         return btn
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        textField?.delegate = self
-        textField?.addTarget(self, action: #selector(editingChanged(_:)), for: .editingChanged)
-        textField?.autocorrectionType = .no
-        button?.isEnabled = textField?.text != nil
-        view.backgroundColor = PPLColor.primary
-        textField?.backgroundColor = PPLColor.quaternary
-        textField?.becomeFirstResponder()
-        label?.textColor = UIColor.white
-        textField?.superview?.sizeToFit()
-        button?.superview?.sizeToFit()
+    func buttonConfig() -> UIButton.Configuration {
+        var config = UIButton.Configuration.borderedProminent()
+        config.baseBackgroundColor = .black
+        config.buttonSize = UIButton.Configuration.Size.large
+        return config
     }
     
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        button?.isEnabled = textField?.text != nil
+        textField?.becomeFirstResponder()
     }
     
     @objc func editingChanged(_ sender: Any) {
